773c99339ccb61ada4a28842bc934809ba311d81
Shivam1320 <shivam.skt13@gmail.com>
2024-05-19 15:27:24 +0530

    Apis done

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const TimeAndLocation = () => {
   return (
-    <div>TimeAndLocation</div>
+    <div>
+        <div className="flex items-center justify-center my-6">
+            <p className="text-xl font-extralight">Tuesday, 18 May 2024 | Local time: 07:21 AM</p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-3xl font-medium'>Berlin, DE</p>
+        </div>
+    </div>
   )
 }
 
